67ce7bbaa389d21cc4a95f775fd7c338c0f15a1e
JH-20 <jere.hirviniemi@gmail.com>
2021-03-02 21:22:24 +0200

    Database: Change .query() to .queryArray()

--- database/database.js
+++ database/database.js
@@ -9,7 +9,7 @@ const executeQuery = async(query, ...args) => {
   const client = getClient();
   try {
     await client.connect();
-    return await client.query(query, ...args);
+    return await client.queryArray(query, ...args);
   } catch (e) {
     console.log(e);
   } finally {
